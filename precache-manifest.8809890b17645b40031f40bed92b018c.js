37fea224a28ee19127a18bdd8a97f48fc580d77b
Bruno de Moraes <61841052+theoriginalkaiju@users.noreply.github.com>
2021-03-08 22:56:09 -0300

    Updates

---
+++ precache-manifest.8809890b17645b40031f40bed92b018c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddfdc48fe2f59f9a1c2a6a762944b9d",
+    "url": "/jankenpon/index.html"
+  },
+  {
+    "revision": "2ff708afe11f68366526",
+    "url": "/jankenpon/static/css/2.202fbde5.chunk.css"
+  },
+  {
+    "revision": "2ff708afe11f68366526",
+    "url": "/jankenpon/static/js/2.2cb46829.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/jankenpon/static/js/2.2cb46829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee307071d513ef6ec2a9",
+    "url": "/jankenpon/static/js/main.5263a68a.chunk.js"
+  },
+  {
+    "revision": "bd5d0c0d130e723a2944",
+    "url": "/jankenpon/static/js/runtime-main.6fd4b15e.js"
+  }
+]);
